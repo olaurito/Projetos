55533ea2b618601fa6f053d089a348cc968ddaf3
Olaurito Netto <linkinneto.bsi@gmail.com>
2020-04-02 20:31:57 -0300

    alterações nos bancos

--- banco.php
+++ banco.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once 'src/Modelo/Conta/Conta.php';
+require_once 'src/Modelo/Conta/Titular.php';
+
+$primeiraConta = new Conta(new Titular( '123.456.789-10', 'Olaurito Neto'));
+
+
+echo $primeiraConta->recuperaNumeroDeContas(). PHP_EOL;
+
