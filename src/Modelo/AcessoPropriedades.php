4a84d333babb07b0d6134f508448317420dfc85c
Olaurito Netto <linkinneto.bsi@gmail.com>
2020-04-03 20:34:58 -0300

    add comentarios

--- src/Modelo/AcessoPropriedades.php
+++ src/Modelo/AcessoPropriedades.php
@@ -4,6 +4,12 @@ namespace Alura\Banco\Modelo;
 
 trait AcessoPropriedades
 {
+    /*
+    trait - reutilização de código, sem a necessidade de herança.
+    https://www.php.net/manual/en/language.oop5.traits.php
+    ucfirst(); metodo para colocar em maiscula;
+
+    */
     public function __get(string $nomeAtributo)
     {
         $metodo = 'recupera' . ucfirst($nomeAtributo);
--- src/Modelo/Pessoa.php
+++ src/Modelo/Pessoa.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Alura\Banco\Modelo;
+namespace Alura\Banco\Modelo; // pacotes do php para separar arquivos
 
 abstract class Pessoa
 {
